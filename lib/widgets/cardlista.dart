8c9cf815ec3413c6db044906b8ce956f45e4de54
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-07-05 18:48:51 -0300

    view informations about light

--- lib/widgets/cardlista.dart
+++ lib/widgets/cardlista.dart
@@ -1,42 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:appiluminacao/pages/control.dart';
 
-class MyCard extends StatelessWidget {
+class MyCard extends StatefulWidget {
+  MyCard({super.key, required this.propertiesLights});
+  Map<dynamic, dynamic> propertiesLights;
+  @override
+  State<MyCard> createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ControleMonitoramentoPage()),
-        );
-      },
-      child: Column(
-        children: [
-          Card(
-            color: Colors.blue[200],
-            shadowColor: Color.fromARGB(255, 44, 78, 233),
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListTile(
-              leading:
-                  CircleAvatar(child: Icon(Icons.display_settings_outlined)),
-              title: Text("Sistema 1"),
-              subtitle: Text("Status:"),
-              trailing: Icon(Icons.lightbulb_outline),
+    return Column(
+      children: [
+        Card(
+          color: Colors.blue[200],
+          shadowColor: const Color.fromARGB(255, 44, 78, 233),
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ListTile(
+            leading: const CircleAvatar(
+                child: Icon(Icons.display_settings_outlined)),
+            title: Text("Sistema: ${widget.propertiesLights['id'].toString()}"),
+            subtitle: Text(
+                "Status: ${widget.propertiesLights['status'] == 1 ? 'ligado' : 'desligado'}"),
+            trailing: Icon(
+              Icons.lightbulb,
+              color: widget.propertiesLights['status'] == 1
+                  ? Colors.amber
+                  : Colors.black,
             ),
           ),
-          const Divider(
-            color: Color.fromARGB(255, 31, 29, 29),
-            thickness: 1,
-            height: 10,
-            indent: 20,
-            endIndent: 20,
-          )
-        ],
-      ),
+        ),
+        const Divider(
+          color: Color.fromARGB(255, 31, 29, 29),
+          thickness: 1,
+          height: 10,
+          indent: 20,
+          endIndent: 20,
+        )
+      ],
     );
   }
 }
