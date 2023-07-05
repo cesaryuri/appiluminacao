95ae83df3b9e30347f859418ac8f36ad1e7deed3
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-07-05 18:48:37 -0300

    listing the lamps registered in the bank

--- lib/pages/card_listtile.dart
+++ lib/pages/card_listtile.dart
@@ -1,8 +1,17 @@
+import 'package:appiluminacao/pages/control.dart';
+import 'package:appiluminacao/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:appiluminacao/widgets/cardlista.dart';
 
-class ListCards extends StatelessWidget {
+class ListCards extends StatefulWidget {
   const ListCards({Key? key}) : super(key: key);
+
+  @override
+  State<ListCards> createState() => _ListCardsState();
+}
+
+class _ListCardsState extends State<ListCards> {
+  final apiService = ApiService();
 
   @override
   Widget build(BuildContext context) {
@@ -11,36 +20,43 @@ class ListCards extends StatelessWidget {
         title: const Text("Dispositivos"),
       ),
       body: Center(
-        child: ListView(
-          reverse: false,
-          children: [
-            Column(
-              children: [
-                MyCard(),
-                MyCard(),
-                MyCard(),
-                MyCard(),
-                MyCard(),
-                MyCard(),
-              ],
-            ),
-          ],
+        child: FutureBuilder(
+          future: apiService.getAllligths(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              List<dynamic>? listLights = snapshot.data;
+              return RefreshIndicator(
+                child: ListView.builder(
+                  itemCount: listLights!.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ControleMonitoramentoPage(
+                                propertiesLights: listLights[index],
+                              ),
+                            ),
+                            (route) => false);
+                      },
+                      child: MyCard(
+                        propertiesLights: listLights[index],
+                      ),
+                    );
+                  },
+                ),
+                onRefresh: () {
+                  return Future.delayed(const Duration(seconds: 1), () {
+                    setState(() {});
+                  });
+                },
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
-      ),
-    );
-  }
-
-  SingleChildScrollView singleChildScroll() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-        ],
       ),
     );
   }
